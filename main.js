6bd39ec744f2668cef2ceb52e36fe5c3ddeee4d0
kaustubh-khandwal <76221706+kaustubh-khandwal@users.noreply.github.com>
2021-10-06 13:22:59 +0530

    update main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+nose_x = 0;
+nose_y = 0;
 function setup() {
     canvas = createCanvas(300,300);
     canvas.center();
@@ -9,10 +11,16 @@ function setup() {
     posenet.on("pose",getposes)
 }
 
+function preload() {
+    moustache = loadImage("https://i.postimg.cc/3x3QzSGq/m.png")
+}
+
 function getposes(result) {
     if (result.length > 0) {
         console.log(result)
         console.log("nose x ="+result[0].pose.nose.x)
+        nose_x = result[0].pose.nose.x - 25;
+        nose_y = result[0].pose.nose.y - 5;
     }
 }
 
@@ -22,6 +30,8 @@ function modelloaded() {
 
 function draw() {
     image(video,0,0,300,300);
+    image(moustache,nose_x,nose_y,50,50)
+   
 }
 
 function TakeSnapshot() {
